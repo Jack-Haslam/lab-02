b30306961fda8f8d9e6b24be267a15544e8ff9fe
Jack-Haslam <c00311512@setu.ie>
2025-09-22 15:40:04 +0000

    22/9/25 not finished

--- lab21.py
+++ lab21.py
@@ -1,3 +1,17 @@
+import re
+
 with open('auth.log', 'r') as f:
     for line in f:
         print(line)
+
+ips = []     # creates an empty list called ips
+
+with open('auth.log', 'r') as f:
+    for line in f:
+        # Search for IP addresses in each line
+        found_ips = re.findall(r'\d+\.\d+\.\d+\.\d+', line)
+        ips.extend(found_ips)  # Add found IPs to the list
+
+# Print all collected IP addresses
+for ip in ips:
+    print(ip)
--- lab22.py
+++ lab22.py
@@ -1,13 +1,52 @@
-import re
+# lab2-2_starter.py
 
-ips = []     # creates an empty list called ips
+LOGFILE = "sample_auth_small.log"  # change filename if needed
+""""
+def simple_parser(line):#This function locates the port no.
+    
+    looks for the substring ' port ' and returns the following port number.
+    Returns None if no matching substring found.
+    
+    if " port " in line:
+        parts = line.split() # splits the line into tokens, seperates by spaces by default
+        try:
+            anchor = parts.index("port")    # Find the position of the token "port", our anchor
+            port = parts[anchor+1]          # the port value will be next token, anchor+1
+            return port.strip()             # strip any trailing punctuation
 
-with open('auth.log', 'r') as f:
-    for line in f:
-        # Search for IP addresses in each line
-        found_ips = re.findall(r'\d+\.\d+\.\d+\.\d+', line)
-        ips.extend(found_ips)  # Add found IPs to the list
+        except (ValueError, IndexError):
+            return None
 
-# Print all collected IP addresses
-for ip in ips:
-    print(ip)
+    return None
+    """
+
+def simple_ips(line):#This function locates the ips from "sample_auth_small.log" 
+                         
+    if " from " in line:
+        ips = line.split()                 # splits the line into tokens, seperates by spaces by default
+        try:
+            anchor = ips.index("from")     # Find the position of the token "from", our anchor
+            ips = ips[anchor+1]            # the port value will be next token, anchor+1
+            return ips.strip()             # strip any trailing punctuation
+
+        except (ValueError, IndexError):
+            return None
+    return None
+
+## This is the main block that will run first. 
+## It will call any functions from above that we might need.
+if __name__ == "__main__":
+
+    uniqueIps = ()
+    noOfLines = 0
+
+    with open(LOGFILE, "r") as f:
+
+        for line in f:
+        
+            print (simple_ips(line.strip()))
+            noOfUniqueIps = uniqueIps(simple_ips)
+            noOfLines = noOfLines+1
+
+    print ("Lines read: "+str(noOfLines))
+    print(noOfUniqueIps)
